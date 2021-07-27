df91e8855a125312429ef6209abd897c02956fc9
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-07-27 17:19:02 -0400

    added task input requirement logic

--- day2/app.js
+++ day2/app.js
@@ -5,52 +5,51 @@ const completedTasks = document.getElementById('completedTasks');
 
 // submit task button logic - to Pending Tasks list
 submitTask.addEventListener('click', function () {
-  // task variable
-  const task = addTask.value;
+  if (addTask.value === '') {
+    alert('Please enter your task!');
+  } else {
+    // task variable
+    const task = addTask.value;
 
-  // create li element
-  const taskLi = document.createElement('li');
+    // create li element
+    const taskLi = document.createElement('li');
 
-  // create task checkbox
-  const taskCheck = document.createElement('input');
-  taskCheck.setAttribute('type', 'checkbox');
-  // giving it an id in order to reference it outside of this event
-  taskCheck.setAttribute('id', 'checkBox');
+    // create task checkbox
+    const taskCheck = document.createElement('input');
+    taskCheck.setAttribute('type', 'checkbox');
 
-  // create task span - header
-  const taskTitle = document.createElement('span');
-  taskTitle.innerHTML = task;
+    // create task span - header
+    const taskTitle = document.createElement('span');
+    taskTitle.innerHTML = task;
 
-  // create remove button
-  const rmButton = document.createElement('button');
-  rmButton.innerHTML = 'Remove';
+    // create remove button
+    const rmButton = document.createElement('button');
+    rmButton.innerHTML = 'Remove';
 
-  // Add input, span and button to Li
-  taskLi.appendChild(taskCheck);
-  taskLi.appendChild(taskTitle);
-  taskLi.appendChild(rmButton);
+    // Add input, span and button to Li
+    taskLi.appendChild(taskCheck);
+    taskLi.appendChild(taskTitle);
+    taskLi.appendChild(rmButton);
 
-  pendingTasks.appendChild(taskLi);
+    pendingTasks.appendChild(taskLi);
 
-  // Creating event for checking task box
-  const checkBox = document.getElementById('checkBox');
+    // remove button logic:
+    rmButton.addEventListener('click', function () {
+      const parent = this.parentElement.parentElement;
+      if (parent.id === 'pendingTasks') {
+        pendingTasks.removeChild(this.parentElement);
+      } else {
+        completedTasks.removeChild(this.parentElement);
+      }
+    });
 
-  // remove button logic:
-  rmButton.addEventListener('click', function () {
-    const parent = this.parentElement.parentElement;
-    if (parent.id === 'pendingTasks') {
-      pendingTasks.removeChild(this.parentElement);
-    } else {
-      completedTasks.removeChild(this.parentElement);
-    }
-  });
-
-  // Move between completed and pending
-  checkBox.addEventListener('change', function () {
-    if (checkBox.checked) {
-      completedTasks.appendChild(this.parentElement);
-    } else {
-      pendingTasks.appendChild(this.parentElement);
-    }
-  });
+    //   Move between completed and pending
+    taskCheck.addEventListener('change', function () {
+      if (taskCheck.checked === true) {
+        completedTasks.appendChild(this.parentElement);
+      } else if (taskCheck.checked === false) {
+        pendingTasks.appendChild(this.parentElement);
+      }
+    });
+  }
 });
