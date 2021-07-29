d34ed34c42348a9391f59d5067c135af674591bc
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-07-29 17:41:10 -0400

    added drop down functionality based on news source

--- day4/app.js
+++ day4/app.js
@@ -1,3 +1,4 @@
+// function for displaying news articles
 function listNews(articleArray) {
   const articlesList = articleArray.map((article) => {
     const articleItem = `
@@ -19,24 +20,42 @@ function listNews(articleArray) {
   articleSection.innerHTML = articlesList.join('');
 }
 
+// declaring vars for HTML content
 const pickSource = document.getElementById('pickSource');
 const articleSection = document.getElementById('articleSection');
+
+// creating array for news articles
 const newsArray = news.articles;
+
+// create HTML drop down items - based on sources js file
 const sourceArray = sources.sources;
-
-listNews(newsArray);
-
-// Create Drop Down Items
 const newsSources = sourceArray.map((source) => {
   const newsSource = `
-    <option value="all">View All Articles</option>
-    <option value = "${source.name}">${source.name}</option>
-  `;
+      <option value = "${source.name}">${source.name}</option>
+    `;
   return newsSource;
 });
-pickSource.innerHTML = newsSources.join('');
+pickSource.insertAdjacentHTML('beforeend', newsSources);
 
-const sourceNames = sourceArray.map((source) => {
-  const sourceName = source.name;
-  return sourceName;
+// initial page load - display all news
+listNews(newsArray);
+
+// future event updates based on sources
+pickSource.addEventListener('change', function () {
+  const selectedSource = pickSource.value;
+
+  if (selectedSource === 'all') {
+    listNews(newsArray);
+  } else {
+    const filteredNews = newsArray.filter(
+      (article) => article.source.name === selectedSource
+    );
+    listNews(filteredNews);
+  }
 });
+
+// Below map is to pull news file source names to verify test cases
+// const articleSources = newsArray.map((article) => {
+//   const sourceName = article.source.name;
+//   return sourceName;
+// });
